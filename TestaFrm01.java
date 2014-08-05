83e0275af5aa5fffff671ac354a712d071656820
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2014-08-05 20:06:16 -0300

    Update TestaFrm01.java

--- TestaFrm01.java
+++ TestaFrm01.java
@@ -3,8 +3,7 @@ package aula1;
 public class TestaFrm01 {
   
   public static void main (String[] args){
-    Frm01 tela = new Frm01(); // instância da classe
-    tela.criaTela();  //acessa a classe previamente criada
+    new Frm01(); // instância da classe
   }
   
 }
