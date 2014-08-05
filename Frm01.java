cbb0784566692a58793125c18999a9199dd5a891
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2014-08-05 19:53:02 -0300

    Update Frm01.java

--- Frm01.java
+++ Frm01.java
@@ -1,1 +1,12 @@
-package aula 1;
+package aula1;
+import javax.swing.*;
+
+public class Fm01 {
+  
+  public void criaTela (){
+    JFrame f = new JFrame ();
+    f.setTitle("Cadastro");
+    f.setSize(290,100);
+  }
+  
+}
