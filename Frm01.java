cac0abed9e89dfa89a92e376c14070afdc9ddfdf
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2014-08-05 19:57:36 -0300

    Update Frm01.java

--- Frm01.java
+++ Frm01.java
@@ -5,11 +5,11 @@ public class Fm01 {
   
   public void criaTela (){
     JFrame f= new JFrame();
-    f.setSize(290,100);
+    f.setSize(290,100); // tamanho da janela
     f.setTitle("Cadastro");
     f.setLocation(10,10);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.setVisible(true);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // o que faz quando fechar
+    f.setVisible(true); // ultimo atributo
   }
   
 }
