9e8c5ab9ca00a09052c47459345875a613a4c5f0
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2014-08-05 19:53:31 -0300

    Update Frm01.java

--- Frm01.java
+++ Frm01.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 public class Fm01 {
   
   public void criaTela (){
-    JFrame f = new JFrame ();
-    f.setTitle("Cadastro");
+    JFrame f= new JFrame();
     f.setSize(290,100);
+    f.setTitle("Cadastro");
+    f.setLocation(10,10);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setVisible(true);
   }
   
 }
